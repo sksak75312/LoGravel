262eaf67568c3302d7d253a7d98a6a83e6bcb4e9
Eero <sksak75312@gmail.com>
2025-05-21 00:11:25 +0800

    chore: 新增 Shadcn/UI 套件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -97,6 +98,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Button variant='destructive'>Click Here</Button>
       </footer>
     </div>
   );
