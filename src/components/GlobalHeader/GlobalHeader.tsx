326038fd0371700798afb11ad977de3b16a2ee1c
EeroChiao <eero.chiao@udngroup.com.tw>
2025-05-29 16:41:46 +0800

    style: GlobalHeader 樣式調整

--- src/components/GlobalHeader/GlobalHeader.tsx
+++ src/components/GlobalHeader/GlobalHeader.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function GlobalHeader() {
   return (
-    <header className="container">
-      <div className="flex items-center justify-between px-8 py-4 mt-9 rounded-full shadow-[0_0_3px_rgba(0,0,0,0.50)] sticky top-9">
+    <header className="sticky top-9 container">
+      <div className="mt-9 flex items-center justify-between rounded-full bg-[#fbfbfb] px-8 py-4 shadow-[0_0_3px_rgba(0,0,0,0.50)]">
         <Link href="/">
           <h1 className="font-space-grotesk text-[32px] font-bold text-[#2D232E]">
             LoGravel
