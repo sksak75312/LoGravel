cc3fb12566dee41b88a27b8519f1db848469ee17
EeroChiao <eero.chiao@udngroup.com.tw>
2025-05-29 16:40:37 +0800

    style: 新增 GlobalFooter 元件

---
+++ src/components/GlobalFooter/GlobalFooter.tsx
@@ -0,0 +1,7 @@
+export default function GlobalFooter() {
+  return (
+    <footer className="mt-20 border-t-2 border-black py-4 text-center">
+      <p>© 2025 LoGravel. All Rights Reserved. </p>
+    </footer>
+  );
+}
