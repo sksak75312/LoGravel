aa30a402ec2d6913bb17ebfe9a33c0076b2d563e
Eero <sksak75312@gmail.com>
2025-06-04 23:18:00 +0800

    fix: 修正 NEXT 路徑錯誤
    style: 調整 Header 背景顏色位置

--- src/components/GlobalHeader/GlobalHeader.tsx
+++ src/components/GlobalHeader/GlobalHeader.tsx
@@ -2,21 +2,21 @@ import Link from 'next/link';
 
 export default function GlobalHeader() {
   return (
-    <header className="sticky top-0 shadow-[0_0_3px_rgba(0,0,0,0.50)]">
-      <div className="container flex items-center justify-between bg-[#fbfbfb] py-4">
+    <header className="sticky top-0 bg-[#fbfbfb] shadow-[0_0_3px_rgba(0,0,0,0.50)]">
+      <div className="container flex items-center justify-between py-4">
         <Link href="/">
           <h1 className="font-space-grotesk text-[32px] font-bold text-[#2D232E]">
             LoGravel
           </h1>
         </Link>
         <nav className="flex gap-7">
-          <Link href="/" className="text-xl">
+          <Link href="/" className="text-xl text-black">
             首頁
           </Link>
-          <Link href="programs" className="text-xl">
+          <Link href="/programs" className="text-xl text-black">
             技術文章
           </Link>
-          <Link href="challenges" className="text-xl">
+          <Link href="/challenges" className="text-xl text-black">
             刷刷題
           </Link>
         </nav>
