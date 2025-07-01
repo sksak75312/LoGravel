4395def0dbbedb45c498eab3a668de373e2a1b77
Eero <sksak75312@gmail.com>
2025-07-02 00:29:28 +0800

    feat: 設定單一文章 Metadata，使用 Next generateMetadata function

--- src/app/programs/[slug]/ProgramBlogLayout.tsx
+++ src/app/programs/[slug]/ProgramBlogLayout.tsx
@@ -1,6 +1,29 @@
+import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 import ArticleTOC from '@/components/ArticleTOC';
+import { postWithSlug } from '@/utils/getPostData';
+
+/**
+ * @description 因為是動態取得 Metadata 資訊，所以需要使用 Next 提供的 generateMetadata function
+ */
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+  const { data } = postWithSlug(slug);
+
+  return {
+    title: data.title,
+    description: data.description,
+    keywords: [...data.keywords],
+    openGraph: {
+      images: [data.openGraph],
+    },
+  };
+}
 
 export default async function ProgramBlogLayout({
   children,
