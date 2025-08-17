695e4619ee3277cc6b6cacef58bb921de63d143a
Eero <sksak75312@gmail.com>
2025-08-17 13:50:40 +0800

    feat: 新增 ArticleTOC.client 元件，顯示使用者當前閱讀區塊

--- src/components/ArticleTOC/ArticleTOC.tsx
+++ src/components/ArticleTOC/ArticleTOC.tsx
@@ -1,52 +1,10 @@
 import remarkParse from 'remark-parse';
 import { unified } from 'unified';
 import { toc } from 'mdast-util-toc';
-import type { List, RootContent, ListItem, Text } from 'mdast';
+import type { List } from 'mdast';
 
 import { postWithSlug } from '@/utils/getPostData';
-
-/**
- * @description 遞迴函式，用於生成 TOC 樣式
- * @param {toc} MDAST
- * @param {isRoot}
- * @returns JSX Element
- */
-function renderTOC(toc: List, isRoot = false) {
-  if (!toc.children) return null;
-
-  return (
-    <ul className={isRoot ? '' : 'pl-4'}>
-      {toc.children.map((tocItem: ListItem) => {
-        /**
-         * paragraph 變數從 tocItem find 類型是否為 paragraph
-         * link 變數從 paragraph find 類型是否為 link
-         * title 變數從 link children 取得 value
-         * url 變數從 link 取得 url 的值
-         */
-        const paragraph = tocItem.children.find(
-          (n: RootContent) => n.type === 'paragraph',
-        );
-        const link = paragraph!.children.find(
-          (n: RootContent) => n.type === 'link',
-        );
-
-        const title = (link!.children[0] as Text).value;
-        const url = link!.url;
-
-        const subList = tocItem.children.find(
-          (n: RootContent) => n.type === 'list',
-        );
-
-        return (
-          <li key={title}>
-            <a href={url}>{title}</a>
-            {subList && renderTOC(subList)}
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
+import ArticleTOCClient from './ArticleTOC.client';
 
 /**
  *
@@ -70,5 +28,5 @@ export default async function ArticleTOC({
 
   const { map } = toc(tree);
 
-  return <aside {...props}>{renderTOC(map!, true)}</aside>;
+  return <ArticleTOCClient map={map as List} {...props} />;
 }
